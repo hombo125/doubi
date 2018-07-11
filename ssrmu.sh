810311bd71ae4137c18c6fe63fa57d852f2788c0
hombo125 <40883061+hombo125@users.noreply.github.com>
2018-07-12 00:05:48 +0800

    Update ssrmu.sh

--- ssrmu.sh
+++ ssrmu.sh
@@ -894,10 +894,10 @@ JQ_install(){
 		cd "${ssr_folder}"
 		if [[ ${bit} = "x86_64" ]]; then
 			mv "jq-linux64" "jq"
-			#wget --no-check-certificate "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64" -O ${jq_file}
+			#wget --no-check-certificate "https://github.com/hombo125/shadowsocksr/raw/master/jq-linux64" -O ${jq_file}
 		else
 			mv "jq-linux32" "jq"
-			#wget --no-check-certificate "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux32" -O ${jq_file}
+			#wget --no-check-certificate "https://github.com/hombo125/shadowsocksr/raw/master/jq-linux32" -O ${jq_file}
 		fi
 		[[ ! -e ${jq_file} ]] && echo -e "${Error} JQ解析器 重命名失败，请检查 !" && exit 1
 		chmod +x ${jq_file}
