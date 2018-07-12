c08551818c80f1d72d6c868c9de7c403005a38c0
hombo125 <40883061+hombo125@users.noreply.github.com>
2018-07-12 23:04:47 +0800

    Update ssrmu.sh

--- ssrmu.sh
+++ ssrmu.sh
@@ -1815,7 +1815,7 @@ elif [[ "${action}" == "monitor" ]]; then
 	crontab_monitor_ssr
 else
 	echo -e "  ShadowsocksR MuJSON一键管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
-  ---- Toyo | doub.io/ss-jc60 ----
+  ---- hombo125 | QQ群:55134079 ----
 
   ${Green_font_prefix}1.${Font_color_suffix} 安装 ShadowsocksR
   ${Green_font_prefix}2.${Font_color_suffix} 更新 ShadowsocksR
