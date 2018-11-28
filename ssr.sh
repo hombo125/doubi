8bd3429f2071a7bc1039a4d4cec1c96bb5476e4d
hombo125 <40883061+hombo125@users.noreply.github.com>
2018-11-28 12:17:07 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -604,7 +604,7 @@ Debian_apt(){
 # 下载 ShadowsocksR
 Download_SSR(){
 	cd "/usr/local/"
-	wget -N --no-check-certificate "https://github.com/ToyoDAdoubi/shadowsocksr/archive/manyuser.zip"
+	wget -N --no-check-certificate "https://github.com/hombo125/shadowsocksr.git"
 	#git config --global http.sslVerify false
 	#env GIT_SSL_NO_VERIFY=true git clone https://github.com/hombo125/shadowsocksr.git
 	#[[ ! -e ${ssr_folder} ]] && echo -e "${Error} ShadowsocksR服务端 下载失败 !" && exit 1
